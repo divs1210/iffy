7c44820199927b1109bbbeef1f5f2dda01d6191d
Divyansh Prakash <divyansh.prakash@yahoo.com>
2018-11-17 14:48:25 +0530

    Refactor tests

--- test/iffy/core_test.clj
+++ test/iffy/core_test.clj
@@ -26,40 +26,24 @@
 
 
 (deftest DurableStack-tests
-  (let [s (DurableStack.)
-        f "DurableStack-test"]
-    (.setFile s f)
+  (let [f "DurableStack-test"
+        s (doto (DurableStack.)
+            (.setFile f))
+        z (doto (DurableStack.)
+            (.setFile f))]
 
-    (is (empty? (.get s :stack))
-        "fresh stack is empty")
-
-    (testing "push"
-      (.push s 1)
-      (is (= [1] (.get s :stack))
-          ".push puts x on stack")
-
-      (.push s 2)
-      (is (= [2 1] (.get s :stack))
-          ".push puts new item on top"))
-
-    (testing "pop"
-      (is (= 2 (.pop s))
-          ".pop returns item on top")
-
-      (is (= [1] (.get s :stack))
-          ".pop removes item on top"))
-
-    (testing "persistence"
-      (doseq [x (random-sample 0.2 (range 100))]
+    (testing "inheritence"
+      (doseq [x (take 10 (shuffle (range 100)))]
         (.push s x))
 
+      (is (= 10 (count (.get s :stack)))
+          "superclass works as expected"))
+
+    (testing "persistence"
       (.save s)
-
-      (let [s2 (DurableStack.)]
-        (.setFile s2 f)
-        (.load s2)
-
-        (is (= (.get s :stack)
-               (.get s2 :stack))))
+      (.load z)
+      (is (= (.get s :stack)
+             (.get z :stack))
+          ".save and .load work as expected")
 
       (f/delete-file f))))
